d39c97d7ca6797471b4a387af514aefe3e33c3bb
Paul <819418426@qq.com>
2022-02-08 23:57:01 +0800

    购物车完成 83

--- src/store/shopcart/index.js
+++ src/store/shopcart/index.js
@@ -24,17 +24,32 @@ const actions ={
         if (result.code ==200) return 'ok'
         else return Promise.reject(new Error('FAILE'))
     },
+    // 删除全部选中
     deleteAllCheckedCart({dispatch,getters}){
         // context：小仓库，commit【提交mutationa修改state】
         // 获取购物车车中全部的产品（是一个数组）
         let PromiseAll = []
-        getters.cartList.cartInfoList.filter((element)=>element.isChecked ==1).forEach(element => {
-            let promise = dispatch('deleteCartListBySkuId', element.skuId)
-            PromiseAll.push(promise)
-        });
+        
+            getters.cartList.cartInfoList.filter((element)=>element.isChecked ==1).forEach(element => {
+                let promise = dispatch('deleteCartListBySkuId', element.skuId)
+                PromiseAll.push(promise)
+            });
+        
         
         // 只要全部的p1|p2... 都成功,返回结果即为成功
         // 如果有一个失败，返回即为失败结果
+        return Promise.all(PromiseAll)
+    },
+    // 
+    updateAllCartIsChecked({dispatch,getters},isChecked){
+        let PromiseAll = []
+        // 过滤并得到所有isChecked为0 的数组
+        getters.cartList.cartInfoList.forEach((element)=>{
+            // 派发actions并通知更改商品状态
+            let promise = dispatch('changeCheckStatus', {skuId:element.skuId,isChecked})
+            PromiseAll.push(promise)
+        })
+        // 最终返回的结果
         return Promise.all(PromiseAll)
     }
 }
